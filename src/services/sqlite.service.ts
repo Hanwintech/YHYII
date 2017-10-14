e7aa760c0c551defca8b9409ed8bad41e9d2215c
Zoey417 <2538334542@qq.com>
2017-10-14 16:27:54 +0800

    sqlite 删减函数

--- src/services/sqlite.service.ts
+++ src/services/sqlite.service.ts
@@ -17,6 +17,7 @@ export class SqlService {
             this.initDB();
         }, (error) => { return });
     }
+    
     initDB() {
         this.sqlite.create({
             name: 'data.db',
@@ -34,132 +35,12 @@ export class SqlService {
             .catch(e => console.error(e));
     }
 
-    selectData(sql): Observable<string> {
-        this.initDB();
-        return Observable.create(observable => {
-            this.sqlitePorter.importSqlToDb(this.database, sql).then((res) => { console.log(res); observable.next(res); })
-                .catch(e => { console.error(e); observable.next(e) });
-
-        });
-    }
-
-
-    initialAreaData(dataId, dataName) {
-        console.log("initialAreaData");
-        this.initDB();
-        var InsertQuery = "INSERT INTO area (ID,areaName) VALUES ('" + dataId + "','" + dataName + "')";
-        this.database.open().then((data) => {
-            this.database.executeSql("CREATE TABLE IF NOT EXISTS area (ID,areaName)", {}).then((data) => {
-                this.database.executeSql(InsertQuery, {}).then((data) => {
-                    console.log("create area")
-                }, (error) => {
-                    console.log(error);
-                })
-            }, (error) => {
-                console.log(error);
-            })
-        });
-
-    }
-    initialSceneryData(dataId, dataName, InspectAreaID) {
-        this.initDB();
-        var InsertQuery = "INSERT INTO Scenery (ID,sceneryName,InspectAreaID) VALUES ('" + dataId + "','" + dataName + "','" + InspectAreaID + "')";
-        this.database.open().then((data) => {
-            this.database.executeSql("CREATE TABLE IF NOT EXISTS Scenery (ID,sceneryName,InspectAreaID)", {}).then((data) => {
-                this.database.executeSql(InsertQuery, {}).then((data) => {
-                    console.log("insert Scenery");
-                }, (error) => {
-                    console.log(error);
-                })
-            }, (error) => {
-                console.log(error);
-            })
-        });
-    }
-    initialListAncientArchitecture(dataId, dataName, parentName) {
-        var InsertQuery = "INSERT INTO AncientArchitecture (ID,ancientName,sceneryName) VALUES ('" + dataId + "','" + dataName + "','" + parentName + "')";
-        this.sqlite.create({
-            name: "data.db",
-            location: "default"
-        }).then((db: SQLiteObject) => {
-            db.executeSql("CREATE TABLE IF NOT EXISTS AncientArchitecture (ID,ancientName,sceneryName)", {}).then((data) => {
-                console.log("CREATE AncientArchitecture");
-            }, (error) => {
-                console.log(error);
-            })
-            db.executeSql(InsertQuery, {}).then((data) => {
-                console.log("InsertQuery AncientArchitecture");
-            }, (error) => {
-                console.log(error);
-            })
-        }, (error) => {
-            console.log(error);
-        });
-    }
-
-    initialListDisInspectPosition(ID, PID, PositionName, Type) {
-        var InsertQuery = "INSERT INTO DisInspectPosition (ID,PID,PositionName,Type) VALUES ('" + ID + "','" + PID + "','" + PositionName + "','" + Type + "')";
-        this.sqlite.create({
-            name: "data.db",
-            location: "default"
-        }).then((db: SQLiteObject) => {
-            db.executeSql("CREATE TABLE IF NOT EXISTS DisInspectPosition (ID,PID,PositionName,Type)", {}).then((data) => {
-
-            }, (error) => {
-                console.log(error);
-            })
-            db.executeSql(InsertQuery, {}).then((data) => {
-
-            }, (error) => {
-
-            })
-        }, (error) => {
-        });
-    }
-    initialdetailData() {
-        this.sqlite.create({
-            name: "data.db",
-            location: "default"
-        }).then((db: SQLiteObject) => {
-            db.executeSql("CREATE TABLE IF NOT EXISTS detail (recordId,parentId,location,damamgeDegreeSource,damamgeDegree,workTypeSource,workType,inspectDescription,picUrl,respair,respairDescription,inspectPerson,inspectTime)", {}).then((data) => {
-
-            }, (error) => {
-                console.log(error);
-            })
-        }, (error) => {
-            console.log(error);
-        });
-    }
-
-    insertDetailData(dataSource): Observable<string> {
-        return Observable.create(observer => {
-            var InsertQuery = "INSERT INTO detail (recordId,parentId,location,damamgeDegree,workType,inspectDescription,picUrl,respair,respairDescription,inspectPerson,inspectTime) VALUES ('" +
-                dataSource.recordId + "','" + dataSource.parentId + "','" + dataSource.location + "','" + dataSource.damamgeDegree + "','" +
-                dataSource.workType + "','" + dataSource.inspectDescription + "','" + dataSource.picUrl.join(",") + "','" +
-                dataSource.respair + "','" + dataSource.respairDescription + "','" + dataSource.inspectPerson + "','" + dataSource.inspectTime + "')";
-            this.sqlite.create({
-                name: "data.db",
-                location: "default"
-            }).then((db: SQLiteObject) => {
-                db.executeSql(InsertQuery, {}).then((data) => {
-                    observer.next("1");
-                }, (error) => {
-                    observer.next(error);
-                })
-            }, (error) => {
-                observer.next(error);
-            });
-        });
-    }
-
-
     deleteData() {
         this.sqlite.deleteDatabase({
             name: "data.db",
             location: "default"
         })
     }
-
 
     getSelectData(InsertQuery: string): Observable<string> {
         let arr = [];
